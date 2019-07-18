45b254fb63576b73d6a4c8857909e98642753338
pavan3669 <52204850+pavan3669@users.noreply.github.com>
2019-07-18 15:48:16 +0530

    Add files via upload

---
+++ Practice+18072019.py
@@ -0,0 +1,62 @@
+
+# coding: utf-8
+
+# In[ ]:
+
+#Modules: Used to hide core business logic
+
+
+# In[4]:
+
+import numpy as np
+import math_logic
+
+x = int(input("Enter the value of x: "))
+y = int(input("Enter the value of y: "))
+
+vec1 = np.array([x,y])
+vec2 = np.array([-x,-y])
+
+answer1 = math_logic.add(x,y)
+answer2 = math_logic.sub(x,y)
+answer3 = math_logic.mult(x,y)
+
+print(answer1)
+print(answer2)
+print(answer3)
+
+
+# In[ ]:
+
+
+
+
+# In[ ]:
+
+
+
+
+# In[ ]:
+
+
+
+
+# In[ ]:
+
+
+
+
+# In[ ]:
+
+
+
+
+# In[ ]:
+
+
+
+
+# In[ ]:
+
+
+
---
+++ math_logic.py
@@ -0,0 +1,14 @@
+
+# coding: utf-8
+
+# In[ ]:
+
+def add(a,b):
+    return a+b
+
+def sub(a,b):
+    return a-b
+
+def mult(a,b):
+    return a*b
+
