7c8720449679c11643b77953ff995e687a27a3e8
pavan3669 <52204850+pavan3669@users.noreply.github.com>
2019-07-18 15:51:44 +0530

    Update Practice+18072019.py

--- Practice+18072019.py
+++ Practice+18072019.py
@@ -4,6 +4,9 @@
 # In[ ]:
 
 #Modules: Used to hide core business logic
+#A module is a Python object with arbitrarily named attributes that you can bind and reference. 
+#Simply, a module is a file consisting of Python code. A module can define functions, classes and variables. 
+#A module can also include runnable code.
 
 
 # In[4]:
